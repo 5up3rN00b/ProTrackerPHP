a263056f260048adcb5152119929de18808269c6
5up3rN00b <aaron.linear@gmail.com>
2020-06-14 19:48:19 -0700

    Added sidebar for home screen

--- html/index.php
+++ html/index.php
@@ -22,141 +22,145 @@ $passArr = $sth->fetchAll();
 
 $len = sizeof($passArr);
 ?>
-<div id="mapId" style="height: 500px"></div>
-<script>
-    let map = L.map('mapId').setView([39, -98], 5);
+<div class="main">
+    <div id="mapId" style="height: 100%; width: 85%;"></div>
+    <script>
+        let map = L.map('mapId').setView([39, -98], 5);
 
-    let markers = [];
+        let markers = [];
 
-    <?php
-    for ($i = 0; $i < $len; $i++) {
-        echo "markers[$i] = L.marker([" . $passArr[$i]['latitude'] . ',' . $passArr[$i]['longitude'] . "]).addTo(map);";
-        echo "markers[$i].bindPopup('" . "<b>Date:</b>" . $passArr[$i]['date'] . "<br><b>Starting Time:</b>" . $passArr[$i]['starting_time'] . "<br><b>Ending Time:</b>" . $passArr[$i]['ending_time'] . "<br>" . $passArr[$i]['description'] . "');";
-    }
-    ?>
+        <?php
+        for ($i = 0; $i < $len; $i++) {
+            echo "markers[$i] = L.marker([" . $passArr[$i]['latitude'] . ',' . $passArr[$i]['longitude'] . "]).addTo(map);";
+            echo "markers[$i].bindPopup('" . "<b>Date: </b>" . $passArr[$i]['date'] . "<br><b>Starting Time: </b>" . $passArr[$i]['starting_time'] . "<br><b>Ending Time: </b>" . $passArr[$i]['ending_time'] . "<br>" . $passArr[$i]['description'] . "');";
+        }
+        ?>
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    $(function() {
-        $("form#geocodeForm").submit(function(e) {
-            e.preventDefault();
-            var formData = $("form#geocodeForm").serialize();
+        $(function() {
+            $("form#geocodeForm").submit(function(e) {
+                e.preventDefault();
+                var formData = $("form#geocodeForm").serialize();
 
-            $("#dialogDiv").empty();
+                $("#dialogDiv").empty();
 
-            $.ajax({
-                url: "https://nominatim.openstreetmap.org/search",
-                type: 'GET',
-                data: formData,
-                success: function(data) {
-                    item = data[0];
-                    $("#address").val(item.lat + "," + item.lon);
-                    $("#dialogDiv").append(item.display_name);
-                },
-                error: function() {
-                    $("#geocodeForm").html("Error happened");
-                },
-                cache: false,
-                contentType: false,
-                processData: false
+                $.ajax({
+                    url: "https://nominatim.openstreetmap.org/search",
+                    type: 'GET',
+                    data: formData,
+                    success: function(data) {
+                        item = data[0];
+                        $("#address").val(item.lat + "," + item.lon);
+                        $("#dialogDiv").append(item.display_name);
+                    },
+                    error: function() {
+                        $("#geocodeForm").html("Error happened");
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                });
             });
-        });
 
-        var emailTypingTimer, geocodeTypingTimer;
-        var doneTypingInterval = 1000;
+            var emailTypingTimer, geocodeTypingTimer;
+            var doneTypingInterval = 1000;
 
-        $('#email').keyup(function(){
-            clearTimeout(emailTypingTimer);
-            if ($('#email').val()) {
-                emailTypingTimer = setTimeout(emailDoneTyping, doneTypingInterval);
+            $('#email').keyup(function(){
+                clearTimeout(emailTypingTimer);
+                if ($('#email').val()) {
+                    emailTypingTimer = setTimeout(emailDoneTyping, doneTypingInterval);
+                }
+            });
+
+            $('.geocodeForm').each(function() {
+                $(this).keyup(function() {
+                    clearTimeout(geocodeTypingTimer);
+                    if ($(this).val()) {
+                        $('#upload_process').css("display", "none");
+                        geocodeTypingTimer = setTimeout(geocodeDoneTyping, doneTypingInterval * 2);
+                    }
+                })
+            })
+
+            function emailDoneTyping() {
+                $.ajax({
+                    url: "ajax/check.php",
+                    type: "GET",
+                    data: {'email': $('#email').val()},
+                    success: function(data) {
+                        if (data == '0') {
+                            $("#email")[0].setCustomValidity('');
+                        } else {
+                            $("#email")[0].setCustomValidity('This email is already taken.');
+                        }
+                    }
+                });
+            }
+
+            function geocodeDoneTyping() {
+                var formData = $("form#geocodeForm").serialize();
+
+                $("#dialogDiv").empty();
+                $('#upload_process').css("display", "inline");
+
+                $.ajax({
+                    url: "https://nominatim.openstreetmap.org/search",
+                    type: 'GET',
+                    data: formData,
+                    success: function(data) {
+                        let item = data[0];
+                        $("#latitude").val(item.lat);
+                        $("#longitude").val(item.lon);
+                        $("#dialogDiv").append(item.display_name);
+                        $('#upload_process').css("display", "none");
+                    },
+                    error: function() {
+                        $("#geocodeForm").html("Error happened");
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                });
             }
         });
-
-        $('.geocodeForm').each(function() {
-            $(this).keyup(function() {
-                clearTimeout(geocodeTypingTimer);
-                if ($(this).val()) {
-                    $('#upload_process').css("display", "none");
-                    geocodeTypingTimer = setTimeout(geocodeDoneTyping, doneTypingInterval * 2);
-                }
-            })
-        })
-
-        function emailDoneTyping() {
-            $.ajax({
-                url: "ajax/check.php",
-                type: "GET",
-                data: {'email': $('#email').val()},
-                success: function(data) {
-                    if (data == '0') {
-                        $("#email")[0].setCustomValidity('');
-                    } else {
-                        $("#email")[0].setCustomValidity('This email is already taken.');
-                    }
-                }
-            });
-        }
-
-        function geocodeDoneTyping() {
-            var formData = $("form#geocodeForm").serialize();
-
-            $("#dialogDiv").empty();
-            $('#upload_process').css("display", "inline");
-
-            $.ajax({
-                url: "https://nominatim.openstreetmap.org/search",
-                type: 'GET',
-                data: formData,
-                success: function(data) {
-                    let item = data[0];
-                    $("#latitude").val(item.lat);
-                    $("#longitude").val(item.lon);
-                    $("#dialogDiv").append(item.display_name);
-                    $('#upload_process').css("display", "none");
-                },
-                error: function() {
-                    $("#geocodeForm").html("Error happened");
-                },
-                cache: false,
-                contentType: false,
-                processData: false
-            });
-        }
-    });
-</script>
-<form id="geocodeForm" method="get">
-    Street:
-    <input class="geocodeForm" name="street" type="text"> <br>
-    City:
-    <input class="geocodeForm" name="city" type="text"> <br>
-    County:
-    <input class="geocodeForm" name="county" type="text"> <br>
-    State:
-    <input class="geocodeForm" name="state" type="text"> <br>
-    Country:
-    <input class="geocodeForm" name="country" type="text"> <br>
-    <input name="format" value="json" hidden>
-    <input name="limit" value="1" hidden>
-</form>
-<p id="upload_process" style="display: none">Checking <img src="media/loader.gif" width="20" height="20" /></p>
-<p id="dialogDiv"></p>
-<form method="post" action="index.php">
-    Date:<br>
-    <input type="date" name="date"><br>
-    <label for="startingTime">Start Time:</label><br>
-    <input type="time" id="startingTime" name="startingTime"><br>
-    <label for="endingTime">End Time:</label><br>
-    <input type="time" id="endingTime" name="endingTime"><br>
-    <label for="cap">Max size of protesters:</label><br>
-    <input type="text" id="cap" name="cap"><br>
-    <label for="description">Description:</label><br>
-    <input type="text" id="description" name="description"><br>
-    <input type="submit" id="submit" name="submit"><br>
-    <input id="latitude" name="latitude" hidden>
-    <input id="longitude" name="longitude" hidden>
-</form>
+    </script>
+</div>
+<div class="sidenav">
+    <form id="geocodeForm" method="get">
+        Street:
+        <input class="geocodeForm" name="street" type="text"> <br>
+        City:
+        <input class="geocodeForm" name="city" type="text"> <br>
+        County:
+        <input class="geocodeForm" name="county" type="text"> <br>
+        State:
+        <input class="geocodeForm" name="state" type="text"> <br>
+        Country:
+        <input class="geocodeForm" name="country" type="text"> <br>
+        <input name="format" value="json" hidden>
+        <input name="limit" value="1" hidden>
+    </form>
+    <p id="upload_process" style="display: none">Checking <img src="media/loader.gif" width="20" height="20" /></p>
+    <p id="dialogDiv"></p>
+    <form method="post" action="index.php">
+        Date:<br>
+        <input type="date" name="date"><br>
+        <label for="startingTime">Start Time:</label><br>
+        <input type="time" id="startingTime" name="startingTime"><br>
+        <label for="endingTime">End Time:</label><br>
+        <input type="time" id="endingTime" name="endingTime"><br>
+        <label for="cap">Max size of protesters:</label><br>
+        <input type="text" id="cap" name="cap"><br>
+        <label for="description">Description:</label><br>
+        <input type="text" id="description" name="description"><br>
+        <input type="submit" id="submit" name="submit"><br>
+        <input id="latitude" name="latitude" hidden>
+        <input id="longitude" name="longitude" hidden>
+    </form>
+</div>
 <?php
 require '../templates/footer.php';
